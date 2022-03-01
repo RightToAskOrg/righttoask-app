dfffa59f56b95898888c2f0fa199488aed755988
Vanessa Teague <vanessa@thinkingcybersecurity.com>
2022-03-01 15:05:27 +1100

    Configuration for remote server.

--- RightToAskClient/RightToAskClient/RightToAskClient/Constants.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/Constants.cs
@@ -1,20 +1,58 @@
-﻿using Xamarin.Essentials;
+﻿using System.Diagnostics;
+using System.Text.Json;
+using RightToAskClient.Models;
+using Xamarin.Essentials;
 
 namespace RightToAskClient
 {
     public static class Constants
-    { 
-        public static string RegUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:8099/new_registration" : "http://localhost:8099/new_registration/{0}";
+    {
+        private static string ServerConfigFile = "server.config";
+        private static serverConfig serverConf = GetServerConfig();
+        private static string BaseUrl = serverConf.remoteServerUse == "true" ? serverConf.url :
+            DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:8099" : "http://localhost:8099";
+
         // TODO Not quite sure whether the /{0} is needed on iPhones - test.
-        public static string QnUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:8099/new_question" : "http://localhost:8099/new_question/{0}";
-        public static string MPListUrl= DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:8099/MPs.json" : "http://localhost:8099/MPs.json";
-        public static string UserListUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:8099/get_user_list" : "http://localhost:8099/get_user_list";
+        private static string MacExtra = DeviceInfo.Platform == DevicePlatform.Android ? "" : "/{0}";
+        public static string RegUrl = BaseUrl + "/new_registration" + MacExtra;
+        public static string QnUrl = BaseUrl + "/new_question" + MacExtra;
+        public static string MPListUrl = BaseUrl + "MPs.json";
+        public static string UserListUrl = BaseUrl + "get_user_list";
         public static string GeoscapeAPIUrl = "https://api.psma.com.au/beta/v2/addresses/geocoder";
         public static string StoredMPDataFile = "MPs.json";
-		public static string APIKeyFileName = "GeoscapeAPIKey";
+        public static string APIKeyFileName = "GeoscapeAPIKey";
         public static string PublicServerKeyFileName = "PublicServerKey";
 
-        public static string FakePublicKey = "123";
+        // Tries to read server config, returns true and the url if there's a valid configuration file
+        // specifying that that url is to be used.
+        // If the config file doesn't say to use a remote server, or if it can't be read or parsed, default to localhost.
+        private static serverConfig GetServerConfig()
+        {
+            var serialiserOptions = new JsonSerializerOptions();
+            Result<serverConfig> readResult = FileIO.ReadDataFromStoredJson<serverConfig>(ServerConfigFile, serialiserOptions);
 
+            if (!readResult.Err.IsNullOrEmpty())
+            {
+                Debug.WriteLine("Error reading server config file: "+readResult.Err);
+                return new serverConfig{ remoteServerUse = "false", url = ""};
+            }
+
+            if (readResult.Ok.remoteServerUse == "true")
+            {
+                if (!readResult.Ok.url.IsNullOrEmpty())
+                {
+                    return readResult.Ok;
+                }
+            }
+
+            return new serverConfig{ remoteServerUse = "false", url = ""};
+        }
+
+
+        private class serverConfig
+        {
+            public string remoteServerUse = "";
+            public string url ="";
+        }
     }
 }
