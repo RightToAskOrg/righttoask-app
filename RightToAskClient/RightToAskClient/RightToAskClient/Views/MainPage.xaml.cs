e0f0e64f00e420b225bd0b2e1c67391650886899
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2022-03-08 20:23:22 +1100

    Better error-checking and reporting of server config issues.

--- RightToAskClient/RightToAskClient/RightToAskClient/Views/MainPage.xaml.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/Views/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using RightToAskClient.HttpClients;
 using RightToAskClient.Models;
+using RightToAskClient.Resx;
 using Xamarin.Forms;
 
 namespace RightToAskClient.Views
@@ -11,13 +13,18 @@ namespace RightToAskClient.Views
         {
             InitializeComponent();
 
-            // TODO Possibly this should go in OnStart instead
+            // If we don't have a server public key, then there was something wrong with our initialization file.
+            // Although this is not a useful message for users, it's a sufficiently useful message for developers
+            // that it's worth producing here.
+            if (String.IsNullOrEmpty(RTAClient.ServerPublicKey))
+            {
+                DisplayAlert(AppResources.ServerConfigErrorText, AppResources.ServerConfigErrorRecommendation, "", "OK");
+            }
         }
 
         async void OnTop10NowButtonClicked(object sender, EventArgs e)
         {
             App.ReadingContext.TopTen = true;
-            //await Navigation.PushAsync (readingPage);
             await Shell.Current.GoToAsync($"{nameof(ReadingPage)}");
         }
 
