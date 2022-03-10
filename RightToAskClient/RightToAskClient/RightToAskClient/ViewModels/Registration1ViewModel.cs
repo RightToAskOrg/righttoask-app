054d1a1072ba2b7214cadbd8354438b325e9cee0
Matt Lefurge <jackrabbitmsl@gmail.com>
2022-03-09 19:30:50 -0500

    User's Registration info now gets set from the preferences at the app level on startup. Fixes the NoSuchUser error.

--- RightToAskClient/RightToAskClient/RightToAskClient/ViewModels/Registration1ViewModel.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/ViewModels/Registration1ViewModel.cs
@@ -160,8 +160,9 @@ namespace RightToAskClient.ViewModels
             RegisterMPButtonText = AppResources.RegisterMPAccountButtonText;
             RegisterOrgButtonText = AppResources.RegisterOrganisationAccountButtonText;
 
-            // get account info from preferences
-            Registration.display_name =  Preferences.Get("DisplayName", Registration.display_name);
+            // get account info from preferences -- I can probably remove these
+            // now that it's being done at the app level but keeping them shouldn't hurt
+            Registration.display_name = Preferences.Get("DisplayName", Registration.display_name);
             Registration.uid = Preferences.Get("UID", Registration.uid);
             SelectedState = Preferences.Get("State", -1);
 
