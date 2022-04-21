286183cfa8ccce52010438d8c1db99f153b38076
Matt Lefurge <jackrabbitmsl@gmail.com>
2022-04-21 18:29:21 -0400

    OtherUserProfilePage displays Electorates now.

--- RightToAskClient/RightToAskClient/RightToAskClient/ViewModels/OtherUserProfileViewModel.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/ViewModels/OtherUserProfileViewModel.cs
@@ -3,6 +3,7 @@ using RightToAskClient.Models.ServerCommsData;
 using RightToAskClient.Resx;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 
@@ -38,6 +39,13 @@ namespace RightToAskClient.ViewModels
             set => SetProperty(ref _otherUserEmail, value);
         }
 
+        private string _electoratesDisplayString = "";
+        public string ElectoratesDisplayString
+        {
+            get => _electoratesDisplayString;
+            set => SetProperty(ref _electoratesDisplayString, value);
+        }
+
         public OtherUserProfileViewModel()
         {
             // initialize defaults in case we don't hit the message subscriber
@@ -46,6 +54,7 @@ namespace RightToAskClient.ViewModels
             OtherUserUID = "Did Not Get UID";
             OtherUserState = "Did Not Get User's State";
             OtherUserEmail = "No Email Found";
+            ElectoratesDisplayString = "No Electorates Found";
 
             // populate fields based on passsed in user
             MessagingCenter.Subscribe<QuestionViewModel, ServerUser>(this, "OtherUser", (sender, arg) =>
@@ -58,6 +67,19 @@ namespace RightToAskClient.ViewModels
                     OtherUserDisplayName = _sentUser.display_name ?? "No Display Name Found";
                     OtherUserState = _sentUser.state ?? "No State Found";
                     OtherUserEmail = "No Email Available";
+                    if (_sentUser?.electorates != null) // null check necessary due to bypass crash if created user is from the Web
+                    {
+                        List<ElectorateWithChamber> electorates = _sentUser.electorates.ToList();
+                        if (electorates.Any())
+                        {
+                            ElectoratesDisplayString = ""; // reset the string
+                            foreach (ElectorateWithChamber e in electorates)
+                            {
+                                ElectoratesDisplayString += String.Join(", ", e.chamber, e.region);
+                                ElectoratesDisplayString += "\n";
+                            }
+                        }
+                    }
                 }
                 MessagingCenter.Unsubscribe<QuestionViewModel, ServerUser>(this, "OtherUser");
             });
