54abbfde0d7a633c154b0268e99071654214d947
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2022-05-20 19:53:54 +1000

    Some code cleanup, with messages about what to clean up next in NavigationUtils.cs.

--- RightToAskClient/RightToAskClient/RightToAskClient/NavigationUtils.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/NavigationUtils.cs
@@ -12,15 +12,13 @@ namespace RightToAskClient
 {
     public static class NavigationUtils
     {
-
         public static async Task PushMyAnsweringMPsExploringPage()
         {
             string message = "These are your MPs.  Select the one(s) who should answer the question";
-            // var mpsExploringPage = new ExploringPage(App.ReadingContext.ThisParticipant.GroupedMPs,
-            //     App.ReadingContext.Filters.SelectedAnsweringMPsMine, message);
-            // var nextPage = ListMPsFindFirstIfNotAlreadyKnown(mpsExploringPage);
 
-            //TODO** Seems unnecessary if our MPs are not initialized. But actually maybe it's not being pushed...
+            //TODO** Seems unnecessary if our MPs are not initialized. 
+            // Below, don't make the pages that are never used. The code is (somewhat redundant but)
+            // correct but names are confusing.
             var mpsSelectableListPage = new SelectableListPage(App.ReadingContext.Filters.AnsweringMPsListsMine, message, true);
             var nextPage = ListMPsFindFirstIfNotAlreadyKnown(mpsSelectableListPage);
             await Application.Current.MainPage.Navigation.PushAsync(nextPage);
@@ -30,19 +28,17 @@ namespace RightToAskClient
         {
             string message = "These are your MPs.  Select the one(s) who should raise the question in Parliament";
 
-            // var mpsExploringPage = new ExploringPage(App.ReadingContext.ThisParticipant.GroupedMPs,
-            //     App.ReadingContext.Filters.SelectedAskingMPsMine, message);
-            // await LaunchMPFindingAndSelectingPages(mpsExploringPage);
             var mpsSelectableListPage = new SelectableListPage(App.ReadingContext.Filters.AskingMPsListsMine, message, true);
             await LaunchMPFindingAndSelectingPages(mpsSelectableListPage);
         }
 
+        //
         private static async Task LaunchMPFindingAndSelectingPages(SelectableListPage mpsListPage)
         {
             var nextPage = ListMPsFindFirstIfNotAlreadyKnown(mpsListPage);
             await Application.Current.MainPage.Navigation.PushAsync(nextPage);
-            //await Shell.Current.GoToAsync($"{nameof(ExploringPage)}");
         }
+        
         /*
 		 * Either push the list of selectable MPs directly, or push a registration page,
 		 * instructed to push the MPs selection page after.
@@ -67,10 +63,7 @@ namespace RightToAskClient
             string message = "Here is the complete list of MPs - select which one(s) should answer";
             SelectableListPage mpsPage =
                 new SelectableListPage(App.ReadingContext.Filters.AnsweringMPsListsNotMine, message, false);
-            // ExploringPageWithSearch mpsPage
-            //     = new ExploringPageWithSearch(ParliamentData.AllMPs, App.ReadingContext.Filters.SelectedAnsweringMPs, message);
             await Application.Current.MainPage.Navigation.PushAsync(mpsPage);
-            //await Shell.Current.GoToAsync($"{nameof(ExploringPageWithSearch)}");
         }
 
         public static async Task PushAskingMPsNotMineSelectableListPageAsync()
@@ -79,10 +72,7 @@ namespace RightToAskClient
                 "Here is the complete list of MPs - select which one(s) should raise your question in Parliament";
             SelectableListPage mpsPage =
                 new SelectableListPage(App.ReadingContext.Filters.AskingMPsListsNotMine, message, false);
-            // ExploringPageWithSearch mpsPage
-            //    = new ExploringPageWithSearch(ParliamentData.AllMPs, App.ReadingContext.Filters.SelectedAskingMPs, message);
             await Application.Current.MainPage.Navigation.PushAsync(mpsPage);
-            //await Shell.Current.GoToAsync($"{nameof(ExploringPageWithSearch)}");
         }
     }
 }
