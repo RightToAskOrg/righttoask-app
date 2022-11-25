8c84392f18733e9af134ebf45635eb4fb166cbc0
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2022-11-26 09:57:11 +1100

    working through change to JOSResult

--- RightToAskClient/RightToAskClient/RightToAskClient/HttpClients/GeoscapeClient.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/HttpClients/GeoscapeClient.cs
@@ -22,27 +22,34 @@ namespace RightToAskClient.HttpClients
         
         private static readonly GenericHttpClient Client = new GenericHttpClient(SerializerOptions);
 
-        public static async Task<Result<GeoscapeAddressFeature>> GetFirstAddressData(string address)
+        public static async Task<JOSResult<GeoscapeAddressFeature>> GetFirstAddressData(string address)
         {
             var collection = await GetAddressDataAsync(address);
 
-            if (!string.IsNullOrEmpty(collection.Err))
+            if (collection.Failure)
             {
-                return new Result<GeoscapeAddressFeature> { Err = collection.Err };
+                var errorMessage = "Couldn't get first address.";
+                if (collection is ErrorResult<GeoscapeAddressFeatureCollection> errorResult)
+                {
+                    errorMessage = errorResult.Message;
+                }
+                return new ErrorResult<GeoscapeAddressFeature>(errorMessage);
             }
 
-            var addresses = collection.Ok.AddressDataList;
+            // collection.Success
+            var addresses = collection.Data.AddressDataList;
+            
             // I *think* this should never happen, because there should be a message if the
             // address list is empty, but better safe than sorry.
             if(addresses is null || addresses.Length == 0)
             {
-                return new Result<GeoscapeAddressFeature> { Err = "No addresses found" };
+                return new ErrorResult<GeoscapeAddressFeature>("No addresses found");
             }
 
-            return new Result<GeoscapeAddressFeature> { Ok = addresses[0] };
+            return new SuccessResult<GeoscapeAddressFeature>(addresses[0]);
         }
         
-        private static async Task<Result<GeoscapeAddressFeatureCollection>> GetAddressDataAsync(string address)
+        private static async Task<JOSResult<GeoscapeAddressFeatureCollection>> GetAddressDataAsync(string address)
         {
             var requestString = GeoscapeAddressRequestBuilder.BuildRequest(address);
 
@@ -52,7 +59,7 @@ namespace RightToAskClient.HttpClients
 
             if (!string.IsNullOrEmpty(GeoscapeAddressRequestBuilder.ApiKey.Err))
             {
-                return new Result<GeoscapeAddressFeatureCollection>() { Err = GeoscapeAddressRequestBuilder.ApiKey.Err };
+                return new ErrorResult<GeoscapeAddressFeatureCollection>(GeoscapeAddressRequestBuilder.ApiKey.Err);
             }
             
             // The ApiKey.OK _should_ be properly initialised to "" but this isn't guaranteed (despite the
@@ -67,28 +74,30 @@ namespace RightToAskClient.HttpClients
 
         // Geoscape-specific response interpretation.
         // TODO Find out if there's ever >1 message.
-        private static Result<GeoscapeAddressFeatureCollection> InterpretGeoscapeResponse(Result<GeoscapeAddressFeatureCollection> httpResponse)
+        private static JOSResult<GeoscapeAddressFeatureCollection> InterpretGeoscapeResponse(JOSResult<GeoscapeAddressFeatureCollection> httpResponse)
         {
-            if (!string.IsNullOrEmpty(httpResponse.Err))
+            if (httpResponse.Failure)
             {
                 return httpResponse;
             }
-            var errorMessages = httpResponse.Ok.Messages;
             
-            // If there are no messages, the response is OK.
+            // Successful http response, but there still might be errors from Geoscape.
+            var errorMessages = httpResponse.Data.Messages;
+            
+            // If there are no messages, the response is OK. Success.
             if (errorMessages is null || errorMessages.Length == 0 )
             {
-                return new Result<GeoscapeAddressFeatureCollection> { Ok = httpResponse.Ok };
+                return new SuccessResult<GeoscapeAddressFeatureCollection>(httpResponse.Data);
             }
             
             // If there's an error message, prettify the "no address matched" specific error, otherwise
             // just pass the error on.
             if (errorMessages[0] == "No address matched the query.")
             {
-                return new Result<GeoscapeAddressFeatureCollection> { Err = "Address not found - try again." };
+                return new ErrorResult<GeoscapeAddressFeatureCollection>("Address not found - try again.");
             }
 
-            return new Result<GeoscapeAddressFeatureCollection> { Err = errorMessages[0] };
+            return new ErrorResult<GeoscapeAddressFeatureCollection> (errorMessages[0]);
         }
     }
 }
