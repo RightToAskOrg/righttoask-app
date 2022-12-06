53e692746684ac2a10868399f5838cd89224fe8e
George Ostrobrod <george.ostrobrod@thoughtworks.com>
2022-12-06 13:51:09 +1000

    Add Clear and ContainsKey methods to XamarinPreferences

---
+++ RightToAskClient/RightToAskClient/RightToAskClient/Helpers/XamarinPreferences.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+
+namespace RightToAskClient.Helpers
+{
+    public class XamarinPreferences
+    {
+        private Dictionary<string, object> storage = new Dictionary<string, object>();
+
+        public void Clear()
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                storage.Clear();
+            }
+            else
+            {
+                Preferences.Clear();
+            }
+        }
+
+        public bool ContainsKey(string key)
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                return storage.ContainsKey(key);
+            }
+            else
+            {
+                return Preferences.ContainsKey(key);
+            }
+        }
+
+        public string Get(string key, string defaultValue, string? sharedName = null)
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                try
+                {
+                    return (string)storage[key];
+                }
+                catch (KeyNotFoundException)
+                {
+                    return defaultValue;
+                }
+            }
+            else
+            {
+                if (sharedName == null)
+                {
+                    return Preferences.Get(key, defaultValue);
+                }
+                else
+                {
+                    return Preferences.Get(key, defaultValue, sharedName);
+                }
+            }
+        }
+        public bool Get(string key, bool defaultValue, string? sharedName = null)
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                try
+                {
+                    return (bool)storage[key];
+                }
+                catch (KeyNotFoundException)
+                {
+                    return defaultValue;
+                }
+            }
+            else
+            {
+                if (sharedName == null)
+                {
+                    return Preferences.Get(key, defaultValue);
+                }
+                else
+                {
+                    return Preferences.Get(key, defaultValue, sharedName);
+                }
+            }
+        }
+
+        public void Set(string key, string value, string? sharedName = null)
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                storage.Add(key, value);
+            }
+            else
+            {
+                if (sharedName == null)
+                {
+                    Preferences.Set(key, value);
+                }
+                else
+                {
+                    Preferences.Set(key, value, sharedName);
+                }
+            }
+        }
+
+        public void Set(string key, bool value, string? sharedName = null)
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                storage.Add(key, value);
+            }
+            else
+            {
+                if (sharedName == null)
+                {
+                    Preferences.Set(key, value);
+                }
+                else
+                {
+                    Preferences.Set(key, value, sharedName);
+                }
+            }
+        }
+        
+        // XamarinPrefernces.shared.
+        // Preferences.
+        public static XamarinPreferences shared = new XamarinPreferences();
+    }
+}
--- RightToAskClient/UnitTests/Helpers/XamarinPreferencesTests.cs
+++ RightToAskClient/UnitTests/Helpers/XamarinPreferencesTests.cs
@@ -10,7 +10,46 @@ namespace UnitTests.Helpers
         [Fact]
         public void GetSetOneStringTest()
         {
-            var preferences = new XamarinPrefernces();
+            var preferences = new XamarinPreferences();
+            
+            preferences.Set("key0", "stringValue0");
+            var key0value = preferences.Get("key0", "");
+
+            Assert.Equal("stringValue0",key0value);
+        }
+        
+        [Fact]
+        public void GetSetOneBooleanTest()
+        {
+            var preferences = new XamarinPreferences();
+            
+            preferences.Set("key0", true);
+            var key0value = preferences.Get("key0", false);
+
+            Assert.Equal(true, key0value);
+        }
+        
+        [Fact]
+        public void GetStringDefaultValueNoKeyTest()
+        {
+            var preferences = new XamarinPreferences();
+            var key0value = preferences.Get("key0", "default");
+
+            Assert.Equal("default", key0value);
+        }
+        
+        [Fact]
+        public void GetBoolDefaultValueNoKeyTest()
+        {
+            var preferences = new XamarinPreferences();
+            var key0value = preferences.Get("key0", true);
+
+            Assert.Equal(true, key0value);
+        }
+        [Fact]
+        public void GetSetOneStringWithSharedNameTest()
+        {
+            var preferences = new XamarinPreferences();
             
             preferences.Set("key0", "stringValue0", "");
             var key0value = preferences.Get("key0", "", "");
@@ -19,9 +58,9 @@ namespace UnitTests.Helpers
         }
         
         [Fact]
-        public void GetSetOneBooleanTest()
+        public void GetSetOneBooleanWithSharedNameTest()
         {
-            var preferences = new XamarinPrefernces();
+            var preferences = new XamarinPreferences();
             
             preferences.Set("key0", true, "");
             var key0value = preferences.Get("key0", false, "");
@@ -30,21 +69,44 @@ namespace UnitTests.Helpers
         }
         
         [Fact]
-        public void GetStringDefaultValueNoKeyTest()
+        public void GetStringDefaultValueNoKeyWithSharedNameTest()
         {
-            var preferences = new XamarinPrefernces();
+            var preferences = new XamarinPreferences();
             var key0value = preferences.Get("key0", "default", "");
 
             Assert.Equal("default", key0value);
         }
         
         [Fact]
-        public void GetBoolDefaultValueNoKeyTest()
+        public void GetBoolDefaultValueNoKeyWithSharedNameTest()
         {
-            var preferences = new XamarinPrefernces();
+            var preferences = new XamarinPreferences();
             var key0value = preferences.Get("key0", true, "");
 
             Assert.Equal(true, key0value);
+        }
+
+        [Fact]
+        public void ContainsKeyTest()
+        {
+            var preferences = new XamarinPreferences();
+            preferences.Set("key0", true, "");
+            
+            Assert.True(preferences.ContainsKey("key0"));
+            Assert.False(preferences.ContainsKey("key1"));
+        }
+
+        [Fact]
+        public void ClearTest()
+        {
+            var preferences = new XamarinPreferences();
+            preferences.Set("key0", true, "");
+            preferences.Set("key1", "true", "");
+
+            preferences.Clear();
+            
+            Assert.False(preferences.ContainsKey("key0"));
+            Assert.False(preferences.ContainsKey("key1"));
         }
     }
 }
