521041105d00802936bfd02a3face46fb7a2d7d2
George Ostrobrod <george.ostrobrod@thoughtworks.com>
2022-12-16 14:37:50 +1000

    Use own question card layout in AskQuestion

---
+++ RightToAskClient/RightToAskClient/RightToAskClient/Views/AskQuestionPreviewCard.xaml.cs
@@ -0,0 +1,14 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RightToAskClient.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AskQuestionPreviewCard : ContentView
+    {
+        public AskQuestionPreviewCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
