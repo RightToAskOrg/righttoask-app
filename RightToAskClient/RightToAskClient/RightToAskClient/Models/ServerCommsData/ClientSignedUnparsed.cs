13619e030849e3f1135530e966692e198577cde0
Luke McCarthy <lmccarthy@thoughtworks.com>
2022-12-19 14:51:24 +1100

    Add more debugging code.

--- RightToAskClient/RightToAskClient/RightToAskClient/Models/ServerCommsData/ClientSignedUnparsed.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/Models/ServerCommsData/ClientSignedUnparsed.cs
@@ -24,36 +24,38 @@ namespace RightToAskClient.Models.ServerCommsData
             if (user == IndividualParticipant.getInstance().ProfileData.RegistrationInfo.uid)
             {
                 Console.WriteLine("----------------------------------------------------------");
-                Console.WriteLine("valid user");
+                Console.WriteLine("--- valid user");
+                Console.WriteLine("--- current signature: " + signature);
                 var signaturebytes = Convert.FromBase64String(signature);
+                Console.WriteLine("--- decoded signature");
                 Ed25519PublicKeyParameters ClientPublicKey;
                 try
                 {
                     ClientPublicKey = new Ed25519PublicKeyParameters(Convert.FromBase64String(IndividualParticipant.getInstance().ProfileData.RegistrationInfo.public_key));
-                    Console.WriteLine("Got the public key");
+                    Console.WriteLine("--- Got the public key");
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Throw exception: " + e);
+                    Console.WriteLine("--- Throw exception: " + e);
                     Console.WriteLine(e);
                     throw new ArgumentException();
                 }
-                Console.WriteLine("about to get signature");
+                Console.WriteLine("--- about to get signature");
                 var validSig = SignatureVerificationService.VerifySignature(message, signaturebytes, ClientPublicKey);
                 if (validSig)
                 {
-                    Console.WriteLine("valid signature");
+                    Console.WriteLine("--- valid signature");
                     hasInvalidData = false;
                 }
                 else
                 {
-                    Console.WriteLine("invalid signature");
+                    Console.WriteLine("--- invalid signature");
                     hasInvalidData = true;
                 }
             }
             if (string.IsNullOrEmpty(message))
             {
-                Console.WriteLine("has invalid data");
+                Console.WriteLine("--- has invalid data");
                 hasInvalidData = true;
             }
             isValid = !hasInvalidData;
