a784e1630d206a569836e269198da04364ba277e
Luke McCarthy <lmccarthy@thoughtworks.com>
2022-12-20 10:21:51 +1100

    Delete unused validate method in ClientSignedUnparsed.

--- RightToAskClient/RightToAskClient/RightToAskClient/Models/ServerCommsData/ClientSignedUnparsed.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/Models/ServerCommsData/ClientSignedUnparsed.cs
@@ -1,6 +1,3 @@
-using Org.BouncyCastle.Crypto.Parameters;
-using RightToAskClient.CryptoUtils;
-using System;
 using System.Text.Json.Serialization;
 
 namespace RightToAskClient.Models.ServerCommsData
@@ -15,51 +12,5 @@ namespace RightToAskClient.Models.ServerCommsData
 
         [JsonPropertyName("user")]
         public string user { get; set; } = "";
-
-        public bool Validate()
-        {
-            var isValid = false;
-            var hasInvalidData = false;
-            // if user is me, check for valid signature
-            if (user == IndividualParticipant.getInstance().ProfileData.RegistrationInfo.uid)
-            {
-                Console.WriteLine("----------------------------------------------------------");
-                Console.WriteLine("--- valid user");
-                Console.WriteLine("--- current signature: " + signature);
-                var signaturebytes = Convert.FromBase64String(signature);
-                Console.WriteLine("--- decoded signature");
-                Ed25519PublicKeyParameters ClientPublicKey;
-                try
-                {
-                    ClientPublicKey = new Ed25519PublicKeyParameters(Convert.FromBase64String(IndividualParticipant.getInstance().ProfileData.RegistrationInfo.public_key));
-                    Console.WriteLine("--- Got the public key");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("--- Throw exception: " + e);
-                    Console.WriteLine(e);
-                    throw new ArgumentException();
-                }
-                Console.WriteLine("--- about to get signature");
-                var validSig = SignatureVerificationService.VerifySignature(message, signaturebytes, ClientPublicKey);
-                if (validSig)
-                {
-                    Console.WriteLine("--- valid signature");
-                    hasInvalidData = false;
-                }
-                else
-                {
-                    Console.WriteLine("--- invalid signature");
-                    hasInvalidData = true;
-                }
-            }
-            if (string.IsNullOrEmpty(message))
-            {
-                Console.WriteLine("--- has invalid data");
-                hasInvalidData = true;
-            }
-            isValid = !hasInvalidData;
-            return isValid;
-        }
     }
 }
