fc5e42e256fab332c1ec59be3f4e8a36fd752ae3
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2022-12-21 16:54:48 +1100

    Removed commented-out code in Question.cs

--- RightToAskClient/RightToAskClient/RightToAskClient/Models/Question.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/Models/Question.cs
@@ -69,7 +69,6 @@ namespace RightToAskClient.Models
             set
             {
                 SetProperty(ref _background, value);
-                //** QuestionViewModel.Instance.ServerQuestionUpdates.background = _background;
                 Updates.background = _background;
             }
         }
@@ -90,22 +89,17 @@ namespace RightToAskClient.Models
         // TODO do updates.
         public bool AnswerAccepted { get; set; }
         public string IsFollowupTo { get; set; } = "";
-        // public List<string> Keywords { get; set; } = new List<string>();
-        // public List<string> Category { get; set; } = new List<string>();
-        // public DateTime ExpiryDate { get; set; }
         private string _questionId = "";
         public string QuestionId
         {
             get => _questionId;
             set => SetProperty(ref _questionId, value);
-            //** QuestionViewModel.Instance.ServerQuestionUpdates.question_id = _questionId;
         }
         private string _version = "";
         public string Version
         {
             get => _version;
             set => SetProperty(ref _version, value);
-            //** QuestionViewModel.Instance.ServerQuestionUpdates.version = _version;
         }
 
         // The person who suggested the question
@@ -157,7 +151,6 @@ namespace RightToAskClient.Models
         {
             get => _hansardLink;
             private set => SetProperty(ref _hansardLink, value);
-            //** QuestionViewModel.Instance.ServerQuestionUpdates.hansard_link = _hansardLink;
         }
 
         private bool _alreadyDownvoted;
