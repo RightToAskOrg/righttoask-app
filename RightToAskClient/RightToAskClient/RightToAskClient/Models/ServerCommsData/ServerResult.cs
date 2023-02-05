a4ae239c8e1ac00b5c4c4151794bcdf720d9a524
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2023-02-05 17:33:15 +1100

    Add conversion from ServerResults to JOSResults.

--- RightToAskClient/RightToAskClient/RightToAskClient/Models/ServerCommsData/ServerResult.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient/Models/ServerCommsData/ServerResult.cs
@@ -1,13 +1,59 @@
+using System;
+
 namespace RightToAskClient.Models.ServerCommsData
 {
     public class ServerResult<T>
     {
-        // TODO This is non-ideal because I can't declare a type
-        // parameter to be nullable. 
-        // Look at whether more recent versions of the language have nicer ways of doing this,
-        // e.g. https://stackoverflow.com/questions/59702550/c-sharp-8-nullables-and-result-container
-        // Or consider adopting https://josef.codes/my-take-on-the-result-class-in-c-sharp/
+        // Used only for communication with server. For use in app, use JOSResult.
         public T Ok { get; set; }
         public string? Err { get; set; }
+
+        // Converts a ServerResult into the appropriate JOS Result
+        // If there are successive layers or errors, it takes only the first one.
+        // Works only when the nested Results all have the same type parameter. 
+        public JOSResult<T> ToJOSResult()
+        {
+            if (Err is null)
+            {
+                if (Ok is null)
+                {
+                    throw new Exception("Can't make a Data-full JOSResult with no data. Use plain JOSResult instead.");
+                }
+
+                // Recurse for nested results.
+                // This will produce an Ok (with data) if there are Oks all the way down.
+                if (Ok is ServerResult<T> serverResult)
+                {
+                    return serverResult.ToJOSResult();
+                }
+                
+                return new SuccessResult<T>(Ok);
+            }
+            return new ErrorResult<T>(Err);
+        }
+
+
+        public JOSResult ToDatalessJOSResult()
+        {
+            if (Err is null)
+            {
+                if (Ok != null)
+                {
+                    throw new Exception(
+                        "Can't make a Dataless JOSResult with nonempty data. Use JOSResult<T> instead.");
+                }
+
+                // Recurse for nested results.
+                // This will produce an Ok (with data) if there are Oks all the way down.
+                if (Ok is ServerResult<T> serverResult)
+                {
+                    return serverResult.ToDatalessJOSResult();
+                }
+
+                return new SuccessResult();
+            }
+
+            return new ErrorResult(Err);
+        }
     }
 }
