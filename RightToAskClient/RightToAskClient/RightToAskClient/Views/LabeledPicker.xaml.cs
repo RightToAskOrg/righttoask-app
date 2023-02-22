10f4699d2f4c3bb637e06eea1ce38257c55f9124
Juntao Qiu <juntao.qiu@gmail.com>
2023-02-22 13:58:04 +1100

    [Wenping, Juntao] Tried to extract a new component for labeled picker

---
+++ RightToAskClient/RightToAskClient/RightToAskClient/Views/LabeledPicker.xaml.cs
@@ -0,0 +1,14 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RightToAskClient.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabeledPicker : Frame
+    {
+        public LabeledPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
