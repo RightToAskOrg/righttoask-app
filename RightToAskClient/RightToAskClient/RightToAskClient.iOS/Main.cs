3545f6e85639def8fd2153db441c3aa9977847b6
wenping <wenping.du@thoughtworks.com>
2023-03-14 10:59:52 +1030

    Saved DeviceIDs for Android/iOS into Preference

--- RightToAskClient/RightToAskClient/RightToAskClient.iOS/Main.cs
+++ RightToAskClient/RightToAskClient/RightToAskClient.iOS/Main.cs
@@ -1,4 +1,5 @@
-﻿using UIKit;
+﻿using RightToAskClient.Helpers;
+using UIKit;
 
 namespace RightToAskClient.iOS
 {
@@ -7,6 +8,11 @@ namespace RightToAskClient.iOS
         // This is the main entry point of the application.
         private static void Main(string[] args)
         {
+            var id = UIDevice.CurrentDevice.IdentifierForVendor.ToString();
+            if (XamarinPreferences.shared.Get(Constants.DeviceID, "").IsNullOrEmpty())
+            {
+                XamarinPreferences.shared.Set(Constants.DeviceID, id); // save device ID into preference
+            }
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
